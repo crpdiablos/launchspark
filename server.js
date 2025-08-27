6e6596fe2256313eaf5ca8fb39983ff1428db59d
crpdiablos <crpdiablos@gmail.com>
2025-08-27 04:12:38 -0700

    Delete server.js

--- server.js
+++
@@ -1,3 +0,0 @@
-// LaunchSpark Backend v5.0.1 Phoenix (server.js)
-// Placeholder file -- main backend server code goes here (Express app with routes).
-console.log("LaunchSpark backend 5.0.1 running...");
